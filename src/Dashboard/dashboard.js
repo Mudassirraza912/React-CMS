a61d22c8cbab13d21952c6a6af24e96f19cb425b
Mudassir Raza <razamudassir912@gmail.com>
2019-11-02 15:49:33 +0500

    1122019

--- src/Dashboard/dashboard.js
+++ src/Dashboard/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, AppBar, Toolbar, IconButton, Typography, TextField, } from '@material-ui/core'
+import { Button, AppBar, Toolbar, IconButton, Typography, TextField, Card, CardActions, CardContent, useMediaQuery} from '@material-ui/core'
 // import AccountCircle from '@material-ui/icons/AccountCircle';
 // import MailIcon from '@material-ui/icons/Mail';
 // import NotificationsIcon from '@material-ui/icons/Notifications';
@@ -11,8 +11,10 @@ import MenuIcon from '@material-ui/icons/Menu'
 import firebase from '../config/firebase'
 import swal from 'sweetalert'
 import Media from 'react-media';
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-var user = firebase.auth().currentUser;
+
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -24,22 +26,70 @@ class Dashboard extends React.Component {
   }
 
 
+  componentWillMount() {   firebase.auth().onAuthStateChanged((user) => {
+            console.log("USER USER USER", user)
+    });
+  }
+
+
+  logout() {
+      firebase.auth().signOut()
+      .then((success) => {
+          console.log(success)
+      })
+      .catch((err) => {
+        console.log(err)
+    })
+  }
+
   handleClick = () => {
 
     history.push('/signup')
   }
 
   render() {
-      console.log("USER USER USER", user)
     return (
-      <div style={{ height: "100%", width: "100%" }}>
+      <div style={{flex: 1}}>
         <AppBar style={{ backgroundColor: "transparent" }} position="static">
           <Toolbar>
             <Typography onClick={() => { history.push('/') }} style={{ color: "#000" }} variant="h6">
               Website Builder
           </Typography>
-            <div style={{ flex: 1, position: "relative", marginLeft: "50%" }}>
-              <Button onClick={this.handleClick} style={{ marginLeft: "5%"}}><text style={{fontSize:25, fontWeight:"bold", marginRight:5}}>+</text>  Create Page</Button>
+          <div style={{height:"100%", width:"75%"}}>
+          <Media   queries={{
+              small: "(max-width: 599px)",
+              medium: "(min-width: 600px) and (max-width: 1199px)",
+              large: "(min-width: 1200px)"
+            }}>
+              {matches => (
+                <div>
+                  {matches.small ?
+
+                        <div>
+                            </div>
+
+                    :
+
+                    <div style={{ flex: 1, position: "relative",display:"flex", flexDirection:"row", marginLeft:"100%", alignContent:"flex-end" }}>
+<div style={{width:200, flex: 1, height:100}}>
+<Button onClick={this.logout}>logot</Button>
+</div>
+
+
+<div style={{width:200, flex: 1}}>
+  <Button onClick={this.handleClick} style={{ marginLeft: "5%", width:"20%"}}><text style={{fontSize:25, fontWeight:"bold", marginRight:5}}>+</text>  Create Page</Button>
+</div>
+
+</div>
+
+
+
+
+                  }
+
+                </div>
+              )}
+            </Media>
             </div>
           </Toolbar>
         </AppBar>
